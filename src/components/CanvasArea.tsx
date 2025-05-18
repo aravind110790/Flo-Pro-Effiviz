5ae1ed52334d89f6725b274c2e1eba25cfa4dd73
Aravindraj Ramachandran <aravindraj.ramachandran@gmail.com>
2025-05-18 13:56:23 +0000

    I see this error with the app, reported by NextJS, please fix it. The error is reported as HTML but presented visually to the user).
    A > before the line number in the error source usually indicates the line of interest:
    
    > Unhandled Runtime Error: Error: Can't find variable: React. Error source: Call Stack
    > 1
    > map
    > [native code] (0:0)
    >

--- src/components/CanvasArea.tsx
+++ src/components/CanvasArea.tsx
@@ -1,5 +1,5 @@
 
-import type React from 'react';
+import React from 'react';
 import { Card, CardContent } from '@/components/ui/card';
 import { cn } from '@/lib/utils';
 import Image from 'next/image';
--- src/components/SymbolItem.tsx
+++ src/components/SymbolItem.tsx
@@ -1,7 +1,7 @@
 
 'use client';
 
-import type React from 'react';
+import React from 'react';
 import { Card, CardContent } from '@/components/ui/card';
 import { cn } from '@/lib/utils';
 
