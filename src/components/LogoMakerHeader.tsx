bb97974e2ffe755f1064dc64c32cd41dccc76ba5
Aravindraj Ramachandran <aravindraj.ramachandran@gmail.com>
2025-05-19 01:44:58 +0000

    Unable to publish the app

--- src/components/LogoMakerHeader.tsx
+++ src/components/LogoMakerHeader.tsx
@@ -1,4 +1,6 @@
 
+'use client';
+
 import React from 'react';
 import { Button } from '@/components/ui/button';
 import { Download, ShieldCheck } from 'lucide-react'; // Using ShieldCheck as a placeholder for logo icon
